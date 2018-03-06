43bce666b3d56a6afbdc4ffc86432658ccf2db4e
pipopotamasu <ec0156hx@gmail.com>
2018-03-06 21:23:03 +0900

    add Todo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <todo-app></todo-app>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import TodoApp from './components/TodoApp.vue';
 
 @Component({
   components: {
-  HelloWorld,
-  },
+    TodoApp,
+    },
   })
 
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color:#2c3e50;
-  margin-top: 60px;
-}
-</style>
